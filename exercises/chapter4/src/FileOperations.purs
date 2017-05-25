a3fe2500863890b1f6be605f80f86b3b44452f02
Ashutosh Das <das@ashutoshdas.com>
2017-05-25 17:45:19 +0530

    chapter4 solutions

--- exercises/chapter4/src/FileOperations.purs
+++ exercises/chapter4/src/FileOperations.purs
@@ -2,8 +2,10 @@ module FileOperations where
 
 import Prelude
 
-import Data.Path (Path, ls)
-import Data.Array (concatMap, (:))
+import Control.MonadZero
+import Data.Maybe
+import Data.Path
+import Data.Array (concatMap, (:), filter, head)
 
 allFiles :: Path -> Array Path
 allFiles root = root : concatMap allFiles (ls root)
@@ -12,3 +14,21 @@ allFiles' :: Path -> Array Path
 allFiles' file = file : do
   child <- ls file
   allFiles' child
+
+-- Exercise q1
+onlyFiles :: Path -> Array Path
+onlyFiles = filter (\x -> not (isDirectory x)) <<< allFiles'
+
+-- largest and smallest files in the filesystem
+-- Exercise q2
+
+
+-- search for a file by name
+-- Exercise q3
+whereIsHelper :: String -> Maybe Path
+whereIsHelper str = head $ do 
+    p <- allFiles' root
+    child <- ls p
+    guard $ filename child == str
+    pure p
+
